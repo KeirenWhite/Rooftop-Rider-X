7c97030e7c09ea88197b40e0e76038208edc0161
KeirenWhite <143643209+KeirenWhite@users.noreply.github.com>
2025-02-25 14:27:33 -0500

    Dynamic Music Implemented

--- RooftopRiderX/Assets/Scripts/Music.cs
+++ RooftopRiderX/Assets/Scripts/Music.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Music : MonoBehaviour
@@ -10,11 +12,9 @@ public class Music : MonoBehaviour
     public AudioSource audioSourceLow;
     public AudioSource audioSourceMid;
     public AudioSource audioSourceTop;
-    /*public AudioClip lowSpeed;
-    public AudioClip midSpeed;
-    public AudioClip highSpeed;
-    public AudioClip topSpeed;*/
     private float currentSpeed;
+    private float currentTime = 0;
+    public float duration = 2f;
     void Start()
     {
         audioSourceZero.Play();
@@ -32,13 +32,44 @@ public class Music : MonoBehaviour
     private void DynamicMusic()
     {
         currentSpeed = rb.velocity.magnitude;
+        currentTime += Time.deltaTime;
+        audioSourceZero.volume = .5f;
+        audioSourceLow.volume = 0;
+        audioSourceMid.volume = 0;
+        audioSourceTop.volume = 0;
+        float startZero = audioSourceZero.volume;
+        float startLow = audioSourceLow.volume;
+        float startMid = audioSourceMid.volume;
+        float startTop = audioSourceTop.volume;
+        Debug.Log(currentSpeed);
 
-        if(currentSpeed <= 10f)
+        if(currentSpeed >= 0 && currentSpeed <= 1f) //zero speed
         {
-            /*AudioListener.FindAnyObjectByType<lowSpeed>() = 10000;
-            audioSourceLow.priority = 0;
-            audioSourceMid.priority = 0;
-            audioSourceTop.priority = 0;*/
+            audioSourceZero.volume = 1;
+            audioSourceLow.volume = Mathf.Lerp(startLow, 0, currentTime / duration);
+            audioSourceMid.volume = Mathf.Lerp(startMid, 0, currentTime / duration);
+            audioSourceTop.volume = Mathf.Lerp(startTop, 0, currentTime / duration); 
+        }
+        if(currentSpeed > 1f && currentSpeed <= 56f) //low speed
+        {
+            audioSourceZero.volume = 0; 
+            audioSourceLow.volume = Mathf.Lerp(startLow, .9f, currentTime / 10f); 
+            audioSourceMid.volume = Mathf.Lerp(startMid, 0, currentTime / duration);
+            audioSourceTop.volume = Mathf.Lerp(startTop, 0, currentTime / duration);
+        }
+        if(currentSpeed > 56f && currentSpeed <= 120f) //moderate speed
+        {
+            audioSourceZero.volume = 0;
+            audioSourceLow.volume = Mathf.Lerp(startLow, 0, currentTime / duration);
+            audioSourceMid.volume = Mathf.Lerp(startMid, .75f, currentTime / duration);
+            audioSourceTop.volume = Mathf.Lerp(startTop, 0, currentTime / duration);
+        }
+        if(currentSpeed > 120f) //top speed
+        {
+            audioSourceZero.volume = 0;
+            audioSourceLow.volume = Mathf.Lerp(startLow, 0, currentTime / duration);
+            audioSourceMid.volume = Mathf.Lerp(startMid, 0, currentTime / duration);
+            audioSourceTop.volume = Mathf.Lerp(startTop, .8f, currentTime / duration);
         }
     }
 }
