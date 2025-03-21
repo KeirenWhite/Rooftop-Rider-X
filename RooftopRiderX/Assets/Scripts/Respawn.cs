26807defc6b15c6bfd3c7ce1ccc2ab6791349ac4
KeirenWhite <143643209+KeirenWhite@users.noreply.github.com>
2025-03-20 23:02:36 -0400

    Respawn and death plane added

---
+++ RooftopRiderX/Assets/Scripts/Respawn.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject bike;
+    public Rigidbody bikeRb;
+    public GameObject respawnPoint;
+    public AudioSource fallSound;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Bike"))
+        {
+            RespawnBike();
+        }
+    }
+    
+    private void RespawnBike()
+    {
+        bikeRb.velocity = Vector3.zero;
+        bike.transform.position = respawnPoint.transform.position;
+        fallSound.Play();
+    }
+    
+}
