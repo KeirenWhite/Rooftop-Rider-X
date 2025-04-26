913298b9965a1dcf36299557393295471cd564b3
Sam <90107194+Sam-B-02@users.noreply.github.com>
2025-04-25 21:02:27 -0400

    trick framework

---
+++ RooftopRiderX/Assets/Scripts/ScriptableObjects/TrickScriptableObject.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/TrickScriptableObject")]
+public class TrickScriptableObject : ScriptableObject
+{
+    /*** Object Variables ***/
+    /* 
+    * bool inAir;
+    * 
+    /*** Tracked Variables ***/
+    /* 
+    * Core variables:
+    * - Velocity
+    *      - float minimumMagnitude;
+    *      - bool trackMoveDirection;
+    *           - Vector3 targetMoveDirection;
+    * - Face direction
+    *      - bool trackFaceDirection
+    *           - Vector3 targetFaceDirection;
+    *           - bool relativeToVelocity;
+    * 
+    * Air variables:
+    * - Rotation
+    *      - bool trackRotation;
+    *           - Vector3 rotationDifference;
+    * - Boost
+    *       - bool trackBoost
+    *           - bool bonusForBoost;
+    *               - float boostMultiplier;
+    *           - bool boostRequired;
+    *               - float boostAmount;
+    * 
+    * Ground variables:
+    * - Drift
+    *       - bool trackDrift
+    *           - float targetDriftTime;
+    *           - float minimumDriftSharpness;
+    * - Ground normal
+    * - Accelerate/Reverse
+    * - Grounded wheels
+    * 
+    * 
+    /*** VariableSpecifics ***/
+
+    bool trackVariable;
+
+    //Note: Hangtime points are applied on bike landing
+
+
+
+
+}
